d7502624250a0f6f94a2897f60549fc0c26e89fa
Kas <kbberendsen@gmail.com>
2024-06-26 14:52:31 +0200

    update machine control logic to handle set temperature

--- pico/machine_control.py
+++ pico/machine_control.py
@@ -41,6 +41,24 @@ def handle_request(request):
                 return f'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n{mode}'.encode('utf-8')
             else:
                 return 'HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain; charset=utf-8\r\n\r\nInvalid mode'.encode('utf-8')
+            
+    # POST set temperature
+    elif path == '/setTemperature':
+        if method == 'POST':
+            content_length = int(request.split('Content-Length: ')[1].split('\r\n')[0])
+            body = request.split('\r\n\r\n')[1][:content_length]
+            params = dict(x.split('=') for x in body.split('&'))
+            temperature = float(params.get('temperature', 0))
+
+            # Validate and set temperature
+            if 90 <= temperature <= 98:
+                if mode == 'coffee':
+                    coffee_pid.setpoint = temperature
+                else:
+                    steam_pid.setpoint = temperature
+                return 'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\n\r\nTemperature set successfully'.encode('utf-8')
+            else:
+                return 'HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain; charset=utf-8\r\n\r\nTemperature out of range'.encode('utf-8')
 
     # GET intitial data (current temp, target temp, mode)
     elif path == '/initialData':
